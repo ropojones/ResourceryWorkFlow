860c9b4568a254864483365f6b75dfd66f5b1e7a
Oluropo Akinbolade-Jones <ropojones@gmail.com>
2026-02-10 13:27:19 +0100

    Additional features added to Resourcery Project

--- apps/ResourceryWorkflow/src/app/interpretation/interpretation.module.ts
+++ apps/ResourceryWorkflow/src/app/interpretation/interpretation.module.ts
@@ -6,6 +6,21 @@ import { InterpretationRoutingModule } from './interpretation-routing.module';
 import { InterpretationComponent } from './interpretation.component';
 import { InterpretationLayoutComponent } from './interpretation-layout.component';
 import { InterpretationSidebarMenuComponent } from './interpretation-sidebar-menu.component';
+import { InterpretationRequestsComponent } from './interpretation-requests/interpretation-requests.component';
+import { InterpretationProcessingComponent } from './interpretation-processing/interpretation-processing.component';
+import { InterpretaterDeploymentPhysicalComponent } from './interpretater-deployment-physical/interpretater-deployment-physical.component';
+import { InterpretaterDeploymentVirtualComponent } from './interpretater-deployment-virtual/interpretater-deployment-virtual.component';
+import { InterpretaterDeploymentHybridComponent } from './interpretater-deployment-hybrid/interpretater-deployment-hybrid.component';
+import { InterpretationInspectionComponent } from './interpretation-inspection/interpretation-inspection.component';
+import { InterpretationSoundCheckPhysicalComponent } from './interpretation-sound-check-physical/interpretation-sound-check-physical.component';
+import { InterpretationSoundCheckHybridComponent } from './interpretation-sound-check-hybrid/interpretation-sound-check-hybrid.component';
+import { InterpretationSoundCheckVirtualComponent } from './interpretation-sound-check-virtual/interpretation-sound-check-virtual.component';
+import { InterpreterPaymentComponent } from './interpreter-payment/interpreter-payment.component';
+import { InterpretationFeedbackComponent } from './interpretation-feedback/interpretation-feedback.component';
+import { ApprovedRequestsComponent } from './approved-requests/approved-requests.component';
+import { InProcessRequestsComponent } from './in-process-requests/in-process-requests.component';
+import { RejectedRequestsComponent } from './rejected-requests/rejected-requests.component';
+import { SlaComponent } from './sla/sla.component';
 import { ResourceryLayoutModule } from '../resourcery/layout/resourcery-layout.module';
 
 
@@ -13,7 +28,22 @@ import { ResourceryLayoutModule } from '../resourcery/layout/resourcery-layout.m
   declarations: [
     InterpretationComponent,
     InterpretationLayoutComponent,
-    InterpretationSidebarMenuComponent
+    InterpretationSidebarMenuComponent,
+    InterpretationRequestsComponent,
+    InterpretationProcessingComponent,
+    InterpretaterDeploymentPhysicalComponent,
+    InterpretaterDeploymentVirtualComponent,
+    InterpretaterDeploymentHybridComponent,
+    InterpretationInspectionComponent,
+    InterpretationSoundCheckPhysicalComponent,
+    InterpretationSoundCheckHybridComponent,
+    InterpretationSoundCheckVirtualComponent,
+    InterpreterPaymentComponent,
+    InterpretationFeedbackComponent,
+    ApprovedRequestsComponent,
+    InProcessRequestsComponent,
+    RejectedRequestsComponent,
+    SlaComponent
   ],
   imports: [
     CommonModule,
